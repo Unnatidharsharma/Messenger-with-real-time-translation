fa04a0d0ceb5487fdae3653527e8698956906205
Unnatidhar Sharma <144002937+Unnatidharsharma@users.noreply.github.com>
2025-09-08 14:39:06 +0530

    Delete Whatsapp-clone-main directory

--- Whatsapp-clone-main/backend/src/main/java/com/whatsappClone/Exception/MessageException.java
+++
@@ -1,9 +0,0 @@
-package com.whatsappClone.Exception;
-
-public class MessageException extends Exception {
-
-    public MessageException(String message) {
-        super(message);
-    }
-
-}
--- Whatsapp-clone-main/backend/src/main/java/com/whatsappClone/config/JwtConstant.java
+++
@@ -1,9 +0,0 @@
-package com.whatsappClone.config;
-
-public class JwtConstant {
-
-    public static final String JWT_HEADER = "Authorization";
-
-    public static final String SECRET_KEY = "jkahlfadskjahfgfadsfadafdsgfdvresdfpiurtharklarjwroajfn,msdfhjaio;fdjasflkjaf;lkdhjfaf";
-
-}
